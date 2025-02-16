809e81f0ed0c9aa9ea6fb7a99864bfae028f87ba
spogozhev <72576045+spogozhev@users.noreply.github.com>
2025-02-16 12:57:52 +0300

    initial commit

---
+++ datetime.h
@@ -0,0 +1,25 @@
+#pragma once
+#include <string>
+#include "timespan.h"
+
+namespace DateTime {
+
+enum class weekday { sunday, monday, tuesday, wednesday, thursday, friday, saturday };
+
+class time {
+	// TODO
+};
+
+class date {
+	// TODO
+};
+
+class datetime {
+	// TODO
+};
+
+class timediff {
+	// TODO
+};
+
+}
